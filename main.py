b8240d7790eab8c465f3e7e3c386e10f8bd2bbdf
sergvbash <bask.sv@gmail.com>
2022-07-19 10:12:22 -0400

    till database

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 from flask import Flask
+from flask import redirect, render_template, url_for
 
+app = Flask(__name__)
 
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
 
 
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    app.run(debug=True, port=5001)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
